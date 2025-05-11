b2d2de00a5ad44847be5d9069f4ce9d33338ffb9
Mohamed Ramadan <romio@gmail.com>
2025-05-11 10:05:09 +0300

    seeding users

--- EventHorizon.DataAccess/Persistence/Seeder.cs
+++ EventHorizon.DataAccess/Persistence/Seeder.cs
@@ -32,19 +32,46 @@ public class Seeder(ApplicationDbContext dbContext,
                     EmailConfirmed = true
                 };
 
-                var createResult = await _userManager.CreateAsync(adminUser, "Admin123*");
+                // Create owner of event user
+                var ownerUser = new ApplicationUser
+                {
+                    UserName = "owner123",
+                    Email = "owner@eventhorizon.com",
+                    PhoneNumber = "01013274668",
+                    EmailConfirmed = true
+                };
 
-                if (createResult.Succeeded)
+                // Create usual user
+                var usualUser = new ApplicationUser
+                {
+                    UserName = "test123",
+                    Email = "test@eventhorizon.com",
+                    PhoneNumber = "01013274668",
+                    EmailConfirmed = true
+                };
+
+                var createAdminResult = await _userManager.CreateAsync(adminUser, "Admin123*");
+                var createOwnerResult = await _userManager.CreateAsync(ownerUser, "Owner123*");
+                var createUserResult = await _userManager.CreateAsync(usualUser, "Test123*");
+
+                // Adding the rules
+                if (createAdminResult.Succeeded && createOwnerResult.Succeeded && createUserResult.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(adminUser, RolesConstant.Admin);
+                    await _userManager.AddToRoleAsync(ownerUser, RolesConstant.Owner);
+                    await _userManager.AddToRoleAsync(usualUser, RolesConstant.User);
+
                 }
                 else
                 {
                     // Optional: log errors
-                    foreach (var error in createResult.Errors)
-                    {
+                    foreach (var error in createAdminResult.Errors)
                         Console.WriteLine($"Error creating admin user: {error.Description}");
-                    }
+                    foreach (var error in createOwnerResult.Errors)
+                        Console.WriteLine($"Error creating owner user: {error.Description}");
+                    foreach (var error in createUserResult.Errors)
+                        Console.WriteLine($"Error creating usual user: {error.Description}");
+
                 }
             }
         }
